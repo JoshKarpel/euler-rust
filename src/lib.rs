0cbdb89fe66c817b9596dc88bd049d6a56ee111e
JoshKarpel <josh.karpel@gmail.com>
2017-10-01 17:52:06 -0500

    Figured out a technique for solving specific problems via command line arguments.
    Solved 001.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,52 @@
+use std::error::Error;
+use std::collections::HashMap;
+use std::time::Instant;
+use std::process;
+
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
+    fn it_works() {}
+}
+
+mod problems;
+
+pub struct Args {
+    pub problem: String,
+}
+
+impl Args {
+    pub fn new(args: &[String]) -> Result<Args, &'static str> {
+        if args.len() < 2 {
+            return Err("Not enough arguments!");
+        }
+        let problem = args[1].clone();
+
+        Ok(Args { problem })
     }
 }
+
+pub fn run(problem: &str) -> Result<(), Box<Error>> {
+    let mut problems = HashMap::new();
+    problems.insert("001", problems::p001::solve);
+
+    let solver = match problems.get(problem) {
+        Some(solver) => solver,
+        None => {
+            println!("Couldn't find solver for problem {}!", problem);
+            process::exit(0)
+        }
+    };
+
+    let start = Instant::now();
+    let answer = solver();
+    let end = Instant::now();
+
+    let duration = end.duration_since(start);
+    let duration_in_sec = duration.as_secs() as f64 + duration.subsec_nanos() as f64 * 1e-9;
+
+    println!("Answer: {}  |  Elapsed Time: {:.6} seconds", answer, duration_in_sec);
+
+    Ok(())
+}
+
