4aa1049136df0767154486675fafeecb2e5cbaab
JoshKarpel <josh.karpel@gmail.com>
2017-11-05 17:53:52 -0600

    Solved problem 3.

---
+++ src/problems/p003.rs
@@ -0,0 +1,27 @@
+fn prime_factorize(&n: &i64) -> Vec<i64> {
+    let mut test: i64 = n;
+    let mut factors: Vec<i64> = Vec::new();
+
+    while test % 2 == 0 {
+        factors.push(2);
+        test /= 2;
+    }
+
+    let mut x: i64 = 3;
+
+    while test > 1 {
+        if test % x == 0 {
+            factors.push(x);
+            test /= x;
+        } else {
+            x += 2;
+        }
+    }
+
+    return factors;
+}
+
+pub fn solve() -> i64 {
+    let factors = prime_factorize(&600851475143);
+    *factors.last().unwrap()
+}
