3fe21d2e5bebd144240ee587528a9a76bc5d9e50
JoshKarpel <josh.karpel@gmail.com>
2017-11-10 19:52:33 -0600

    Finished problem 021.

--- src/problems/p021.rs
+++ src/problems/p021.rs
@@ -5,24 +5,42 @@ fn sum_of_proper_factors(&n: &u64) -> u64 {
     utils::proper_factorization(n).iter().sum()
 }
 
-fn is_pair_amicable(&a: &u64, &b: &u64) -> bool {
-    a == sum_of_proper_factors(&b) && b == sum_of_proper_factors(&a)
+//fn is_pair_amicable(&a: &u64, &b: &u64) -> bool {
+//    a == sum_of_proper_factors(&b) && b == sum_of_proper_factors(&a)
+//}
+
+fn find_amicable_pair(a: u64) -> Option<(u64, u64)> {
+    let b: u64 = sum_of_proper_factors(&a);
+
+    if a == b {
+        return None;
+    }
+
+    if a == sum_of_proper_factors(&b) {
+        return Some((a, b));
+    }
+
+    None
 }
 
 pub fn solve() -> i64 {
-    println!("{}", is_pair_amicable(&220, &284));
-
     let mut amicable_numbers: Vec<u64> = Vec::new();
 
     for a in 1..10000 {
-        for b in a + 1..10000 {
-            if is_pair_amicable(&a, &b) {
-                println!("{}, {}", a, b);
-                amicable_numbers.push(a as u64);
+        match find_amicable_pair(a) {
+            Some((a, b)) => {
+                amicable_numbers.push(a);
+                amicable_numbers.push(b);
             }
+            None => {}
         }
     }
 
-    let answer: u64 = amicable_numbers.iter().sum();
+    amicable_numbers.sort();
+    amicable_numbers.dedup();
+
+    let answer: u64 = amicable_numbers
+        .iter()
+        .sum();
     answer as i64
 }
