80e4f2434da82ea5f7c1870ab050dfb32b3f4b2c
JoshKarpel <josh.karpel@gmail.com>
2017-12-12 19:30:50 -0600

    Cleaned up problem 10.

--- src/problems/p010.rs
+++ src/problems/p010.rs
@@ -1,8 +1,8 @@
 use super::utils;
 
 pub fn solve() -> i64 {
-    let primes = utils::sieve_of_eratosthenes(2000000);
+    let primes = utils::sieve_of_eratosthenes(2_000_000);
 
     primes.iter()
-        .fold(0, |sum, &element| sum + element as i64)
+        .fold(0, |sum, &element| sum + element) as i64
 }
